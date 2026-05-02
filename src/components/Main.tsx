5458913399f696da5aa4e169e7b8e77703b78299
Redoyanul Haque <redoyanul1234@gmail.com>
2026-05-02 08:09:41 +0000

    update avatar image to mine.jpeg

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="/mine.jpeg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
